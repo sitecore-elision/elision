2c17d18a8920cd35a61b10b8c84113aa734dd609
Patrick Delancy <PDelancy@degdigital.com>
2016-05-26 20:52:24 -0500

    updated item link extensions so calling code can be more terse, fixed nuget package references and added missing js file for speak field editor

--- src/Elision.Kernel/Extensions/ItemLinkExtensions.cs
+++ src/Elision.Kernel/Extensions/ItemLinkExtensions.cs
@@ -13,50 +13,38 @@ namespace Elision
         public static string Url(this Item item, UrlOptions options = null)
         {
             if (item == null)
-                throw new ArgumentNullException(nameof(item));
+                return null;
 
             return options != null ? LinkManager.GetItemUrl(item, options) : LinkManager.GetItemUrl(item);
         }
 
         public static string ImageUrl(this Item item, string imageFieldName, int? width = null, int? height = null)
         {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-
-            var imageField = (ImageField)item.Fields[imageFieldName];
+            var imageField = (ImageField)item?.Fields?[imageFieldName];
             return imageField?.MediaItem == null ? string.Empty : imageField.ImageUrl(width, height);
         }
 
         public static string ImageUrl(this Item item, ID imageFieldId, int? width = null, int? height = null)
         {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-
-            var imageField = (ImageField)item.Fields[imageFieldId];
+            var imageField = (ImageField)item?.Fields?[imageFieldId];
             return imageField?.MediaItem == null ? string.Empty : imageField.ImageUrl(width, height);
         }
 
         public static string ImageUrl(this Item item, string imageFieldName, MediaUrlOptions options)
         {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-
-            var imageField = (ImageField)item.Fields[imageFieldName];
+            var imageField = (ImageField)item?.Fields?[imageFieldName];
             return imageField?.MediaItem == null ? string.Empty : imageField.ImageUrl(options);
         }
 
         public static string ImageUrl(this Item item, ID imageFieldId, MediaUrlOptions options)
         {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-
-            var imageField = (ImageField)item.Fields[imageFieldId];
+            var imageField = (ImageField)item?.Fields?[imageFieldId];
             return imageField?.MediaItem == null ? string.Empty : imageField.ImageUrl(options);
         }
 
         public static string MediaUrl(this Item item, ID mediaFieldId, MediaUrlOptions options = null)
         {
-            var targetItem = item.TargetItem(mediaFieldId);
+            var targetItem = item?.TargetItem(mediaFieldId);
             if (targetItem == null)
                 return string.Empty;
 
@@ -68,7 +56,7 @@ namespace Elision
 
         public static string MediaUrl(this Item item, string mediaFieldName, MediaUrlOptions options = null)
         {
-            var targetItem = item.TargetItem(mediaFieldName);
+            var targetItem = item?.TargetItem(mediaFieldName);
             if (targetItem == null)
                 return string.Empty;
 
@@ -80,34 +68,19 @@ namespace Elision
 
         public static Item TargetItem(this Item item, ID linkFieldId)
         {
-            if (item == null)
-            {
-                throw new ArgumentNullException(nameof(item));
-            }
-
-            var linkField = (LinkField)item.Fields[linkFieldId];
+            var linkField = (LinkField)item?.Fields?[linkFieldId];
             return linkField?.TargetItem;
         }
 
         public static Item TargetItem(this Item item, string linkFieldName)
         {
-            if (item == null)
-            {
-                throw new ArgumentNullException(nameof(item));
-            }
-
-            var linkField = (LinkField)item.Fields[linkFieldName];
+            var linkField = (LinkField)item?.Fields?[linkFieldName];
             return linkField?.TargetItem;
         }
 
         public static string LinkFieldUrl(this Item item, ID linkFieldId)
         {
-            if (item == null)
-                throw new ArgumentNullException(nameof(item));
-            if (ID.IsNullOrEmpty(linkFieldId))
-                throw new ArgumentNullException(nameof(linkFieldId));
-
-            var field = item.Fields[linkFieldId];
+            var field = item?.Fields?[linkFieldId];
             if (field == null)
                 return string.Empty;
 
@@ -117,8 +90,20 @@ namespace Elision
 
         public static string LinkFieldTarget(this Item item, ID fieldId)
         {
-            XmlField field = item.Fields[fieldId];
+            XmlField field = item?.Fields?[fieldId];
             return field?.GetAttribute("target");
+        }
+
+        public static string LinkFieldClass(this Item item, string fieldName)
+        {
+            XmlField field = item?.Fields?[fieldName];
+            return field?.GetAttribute("class");
+        }
+
+        public static string LinkFieldDescription(this Item item, string fieldName)
+        {
+            XmlField field = item?.Fields?[fieldName];
+            return field?.GetAttribute("description");
         }
     }
 }
