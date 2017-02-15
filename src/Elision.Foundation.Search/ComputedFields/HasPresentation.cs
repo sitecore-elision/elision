3261936993bbfc60b9437ca78677bc5e9f8494ff
Jeffrey <jrondeau@degdigital.com>
2017-02-15 16:07:25 -0600

    Create elision search project and add computed fields

---
+++ src/Elision.Foundation.Search/ComputedFields/HasPresentation.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+
+namespace Elision.Foundation.Search.ComputedFields
+{
+    public class HasPresentation : IComputedIndexField
+    {
+        public string FieldName { get; set; }
+
+        public string ReturnType { get; set; }
+
+        public object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+            if (item?.Visualization.Layout != null)
+                return true;
+
+            return null;
+        }
+    }
+}
